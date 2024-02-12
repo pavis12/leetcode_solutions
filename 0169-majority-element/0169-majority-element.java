27ad2a2f30b9a28c37e54b1e1435ccebf487b162
pavis12 <134910711+pavis12@users.noreply.github.com>
2024-02-12 20:09:36 +0530

    Time: 2 ms (78.11%), Space: 51.1 MB (5.71%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,8 +1,9 @@
 class Solution {
     public int majorityElement(int[] n) {
-        int l=n.length;
-        int e=0,c=0;
-        for(int i=0;i<l;i++){
+        int c=0;
+        int e=0;
+        
+        for(int i=0;i<n.length;i++){
             if(c==0){
                 e=n[i];
                 c++;
@@ -14,22 +15,6 @@ class Solution {
                 c--;
             }
         }
-        int g=0;
-        for(int i=0;i<l;i++){
-            if(e==n[i]){
-                g++;
-            }
-        }
-        /*int d=0;
-        if(d%2==0){
-            d=(n/2)+1;
-        }
-        else{
-            d=n/2;
-        }
-        if(g>d){
-            return e;
-        }*/
         return e;
         
         

940e330e9c2ad0d9cce4f327b439b1df9d223856
Pavithra Sathish <134910711+pavis12@users.noreply.github.com>
2024-10-30 09:29:33 +0530

    Time: 1 ms (99.83%), Space: 53 MB (50.08%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,19 +1,16 @@
 class Solution {
     public int majorityElement(int[] n) {
-        int c=0,a=0;
+        int c=0,m=0;
+        int l=n.length;
         for(int i:n){
-            if(c==0){
-                a=i;
-                c++;
-            }
-            else if(i==a){
-                c++;
-            }
-            else{
-                c--;
-                
-            }
+           if(c==0){
+            m=i;
+            c++;
+           }
+           else if(m==i)c++;
+           else c--;
+            
         }
-        return a;
+        return m;
     }
 }

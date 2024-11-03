8bafe0632b508d53ea2ce973c7fc4490cb3877a8
Pavithra Sathish <134910711+pavis12@users.noreply.github.com>
2024-11-03 11:39:33 +0530

    Time: 0 ms (100%), Space: 42 MB (75.59%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,28 +1,27 @@
 class Solution {
     public void rotate(int[][] m) {
-        for(int i=0;i<m.length;i++){
-            for(int j=0;j<m[0].length;j++){
-                if(i==j){
-                    continue;
-                }
-                else if(i<j){
+        int r=m.length;
+        int c=m[0].length;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(i<j){
                     int t=m[i][j];
                     m[i][j]=m[j][i];
                     m[j][i]=t;
                 }
             }
         }
-        for(int r[]:m){
-            rev(r);
+        for(int row[]:m){
+            rev(row);
         }
+        
     }
-    public void rev(int a[]){
-        int i=0;
-        int j=a.length-1;
-        while(i<=j){
-            int t=a[i];
-            a[i]=a[j];
-            a[j]=t;
+    public void rev(int []r){
+        int i=0,j=r.length-1;
+        while(i<j){
+            int t=r[i];
+            r[i]=r[j];
+            r[j]=t;
             i++;
             j--;
         }

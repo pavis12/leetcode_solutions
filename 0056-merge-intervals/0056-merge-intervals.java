d6e8a376b50128e4e188cddd36adf4bc9bb383cc
Pavithra Sathish <134910711+pavis12@users.noreply.github.com>
2024-11-08 08:27:32 +0530

    Time: 13 ms (7.74%), Space: 46.5 MB (40.8%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,28 +1,36 @@
 class Solution {
-    public int[][] merge(int[][] a) {
-
-        Arrays.sort(a,new Comparator<int[]>(){
-            public int compare(int a[],int[]b){
-                return a[0]-b[0];
+    public int[][] merge(int[][] arr) {
+        int n = arr.length; // size of the array
+        //sort the given intervals:
+        Arrays.sort(arr, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return a[0] - b[0];
             }
         });
-        List<List<Integer>> l=new ArrayList<>();
-        for(int i=0;i<a.length;i++){
-           if(l.isEmpty()||a[i][0]>l.get(l.size()-1).get(1)){
-                l.add(Arrays.asList(a[i][0],a[i][1]));
-           }
-           else{
-            //here set(1) says which idex to change
-            l.get(l.size()-1).set(1,Math.max(a[i][1],l.get(l.size()-1).get(1)));
-           }
+
+        List<List<Integer>> ans = new ArrayList<>();
+        
+
+        for (int i = 0; i < n; i++) {
+            // if the current interval does not
+            // lie in the last interval:
+            if (ans.isEmpty() || arr[i][0] > ans.get(ans.size() - 1).get(1)) {
+                ans.add(Arrays.asList(arr[i][0], arr[i][1]));
+            }
+            // if the current interval
+            // lies in the last interval:
+            else {
+                ans.get(ans.size() - 1).set(1,
+                                            Math.max(ans.get(ans.size() - 1).get(1), arr[i][1]));
+            }
         }
-        int r[][]=new int[l.size()][2];
-        int i=0;
-        for(List<Integer> p:l){
-            r[i][0]=p.get(0);
-            r[i][1]=p.get(1);
-            i++;
+        //return ans;
+        int g[][]=new int[ans.size()][2];
+        for(int i=0;i<ans.size();i++){
+            //List<Integer>r=ans.get(i);
+            g[i][0]=ans.get(i).get(0);
+            g[i][1]=ans.get(i).get(1);
         }
-        return r;
+        return g;
     }
 }

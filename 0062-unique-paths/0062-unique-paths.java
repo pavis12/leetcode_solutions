4e7ad124d9d736913cbfbed33cf8b93b8968b966
Pavithra Sathish <134910711+pavis12@users.noreply.github.com>
2024-12-15 16:56:01 +0530

    Time: 0 ms (100%), Space: 40.3 MB (56.93%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,26 +1,23 @@
 class Solution {
+    public int s=0;
     public int uniquePaths(int m, int n) {
+        
         int d[][]=new int[m][n];
-        for(int []r:d){
+        for(int r[]:d){
             Arrays.fill(r,-1);
         }
-        return find(m-1,n-1,d);
-        
+        return f(m,n,m-1,n-1,d);
+        //return s;
     }
-    public int find(int i,int j,int [][]d){
-        if(i==0&&j==0){
-            
+    public int f(int m,int n,int r,int c,int [][]d){
+        if(r<0||c<0||r>=m||c>=n)return 0;
+        if(d[r][c]!=-1)return d[r][c];
+        if(r==0&&c==0){
             return 1;
         }
-        if(i<0||j<0){
-            return 0;
-        }
-        if(d[i][j]!=-1){
-            return d[i][j];
-        }
-        int l=find(i,j-1,d);
-        int t=find(i-1,j,d);
-        d[i][j]=l+t;
-        return d[i][j];
-    } 
+        int l=f(m,n,r-1,c,d);
+        int ri=f(m,n,r,c-1,d);
+        return d[r][c]=l+ri;
+    }
+
 }

9a1bbfc48b8854d964ce65a093768a66acfe9526
Pavithra Sathish <134910711+pavis12@users.noreply.github.com>
2025-02-15 09:24:51 +0530

    Time: 38 ms (60.68%), Space: 59 MB (48.32%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -40,13 +40,4 @@ class Solution {
         }
         return sum;
     }
-
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        int[] nums1 = {18, 43, 36, 13, 7};
-        int[] nums2 = {10, 12, 19, 14};
-
-        System.out.println(sol.maximumSum(nums1)); // Output: 54
-        System.out.println(sol.maximumSum(nums2)); // Output: -1
-    }
 }

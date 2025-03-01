57f2a4d06d1252af86a554b65bd0c3f2e72ce7fa
Pavithra Sathish <134910711+pavis12@users.noreply.github.com>
2025-03-01 07:28:39 +0530

    Time: 1 ms (96.37%), Space: 43.9 MB (44.46%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -1,18 +1,29 @@
-class Solution {
-    public int[] applyOperations(int[] n) {
-        int l=n.length;
-        int a[]=new int[l];
-        int j=0;
-        int c=0;
-        for(int i=0;i<l-1;i++){
-            if(n[i]==n[i+1]){
-                n[i]*=2;
-                n[i+1]=0;
-                System.out.println(n[i]+" "+n[i+1]);
+public class Solution {
+
+    public int[] applyOperations(int[] nums) {
+        int n = nums.length;
+
+        // Step 1: Apply operations on the array
+        for (int index = 0; index < n - 1; index++) {
+            if (nums[index] == nums[index + 1] && nums[index] != 0) {
+                nums[index] *= 2;
+                nums[index + 1] = 0;
             }
-            if(n[i]!=0)a[j++]=n[i];
         }
-        a[j++]=n[l-1];
-        return a;
+
+        // Step 2: Shift non-zero elements to the beginning
+        int nonZeroIndex = 0;
+        for (int iterateIndex = 0; iterateIndex < n; iterateIndex++) {
+            if (nums[iterateIndex] != 0) {
+                nums[nonZeroIndex++] = nums[iterateIndex];
+            }
+        }
+
+        // Step 3: Fill the remaining positions with zeros
+        while (nonZeroIndex < n) {
+            nums[nonZeroIndex++] = 0;
+        }
+
+        return nums;
     }
 }

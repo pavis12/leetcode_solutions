af11dba64ad6f3aabd98778dca807d00f4c43390
Pavithra Sathish <134910711+pavis12@users.noreply.github.com>
2025-03-12 08:39:57 +0530

    Time: 1 ms (97.57%), Space: 42.8 MB (94.49%) - LeetHub

--- 3463-alternating-groups-i/3463-alternating-groups-i.java
+++ 3463-alternating-groups-i/3463-alternating-groups-i.java
@@ -1,15 +1,10 @@
 class Solution {
     public int numberOfAlternatingGroups(int[] c) {
-        int d[]=new int[c.length+2];
-        for(int i=0;i<d.length;i++){
-            d[i]=c[i%c.length];
-        }
-        System.out.println(d);
+        int n=c.length;
         int p=0;
-        for(int i=1;i<d.length-1;i++){
-            if(d[i]!=d[i-1]&&d[i]!=d[i+1])p++;
+        for(int i=0;i<c.length;i++){
+            if(c[i]!=c[(i+1)%n]&&c[i]!=c[(i-1+n)%n])p++;
         }
         return p;
-
     }
 }

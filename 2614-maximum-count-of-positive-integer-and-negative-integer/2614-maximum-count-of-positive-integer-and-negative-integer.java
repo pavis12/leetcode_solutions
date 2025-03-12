282562bc8f1e2842693a558c2c0aa8c7509fb0d8
Pavithra Sathish <134910711+pavis12@users.noreply.github.com>
2025-03-12 08:22:51 +0530

    Time: 0 ms (100%), Space: 45 MB (63.3%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
@@ -1,17 +1,51 @@
 class Solution {
-    public int maximumCount(int[] n) {
-        int m=Integer.MIN_VALUE;
-        int c=0;
-        for(int i=0;i<n.length;i++){
-            if(n[i]==0)continue;
-            if(n[i]<0){
-                c++;
-            }
-            else{
-                m=Math.max(c,n.length-i);
-                return m;
+
+    // Return the first index where the value is equal to or greater than zero.
+    private int lowerBound(int[] nums) {
+        int start = 0, end = nums.length - 1;
+        int index = nums.length;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (nums[mid] < 0) {
+                start = mid + 1;
+            } else if (nums[mid] >= 0) {
+                end = mid - 1;
+                index = mid;
             }
         }
-        return c;
+
+        return index;
+    }
+
+    // Return the first index where the value is greater than zero.
+    private int upperBound(int[] nums) {
+        int start = 0, end = nums.length - 1;
+        int index = nums.length;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (nums[mid] <= 0) {
+                start = mid + 1;
+            } else if (nums[mid] > 0) {
+                end = mid - 1;
+                index = mid;
+            }
+        }
+
+        return index;
+    }
+
+    public int maximumCount(int[] nums) {
+        // All integers from the first non-zero to last will be positive
+        // integers.
+        int positiveCount = nums.length - upperBound(nums);
+        // All integers from the index 0 to index before the first zero index
+        // will be negative.
+        int negativeCount = lowerBound(nums);
+
+        return Math.max(positiveCount, negativeCount);
     }
 }

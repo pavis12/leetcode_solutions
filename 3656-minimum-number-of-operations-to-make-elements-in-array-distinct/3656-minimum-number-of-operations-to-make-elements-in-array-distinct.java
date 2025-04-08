be1c16d6a3ffacac6b6bf29495c6b725ed7b3ef3
Pavithra Sathish <134910711+pavis12@users.noreply.github.com>
2025-04-08 09:20:10 +0530

    Time: 3 ms (37.26%), Space: 44.1 MB (84.84%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -12,20 +12,15 @@ class Solution {
     }
 
     public boolean isd(int[] n) {
-        int[] copy = Arrays.copyOf(n, n.length); // avoid mutating original array
-        Arrays.sort(copy);
-        for (int i = 0; i < copy.length - 1; i++) {
-            if (copy[i] == copy[i + 1]) {
-                return false;
-            }
+        Set<Integer> set = new HashSet<>();
+        for (int val : n) {
+            if (!set.add(val)) return false;
         }
         return true;
     }
 
     public int[] rm(int[] n) {
-        if (n.length <= 3) {
-            return new int[]{};
-        }
+        if (n.length <= 3) return new int[]{};
         int[] p = new int[n.length - 3];
         for (int i = 3; i < n.length; i++) {
             p[i - 3] = n[i];

1e35b5297c407a8aa4220928877f06f9c5660ddf
Pavithra Sathish <134910711+pavis12@users.noreply.github.com>
2025-04-08 09:21:37 +0530

    Time: 0 ms (100%), Space: 43.6 MB (99.52%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -1,30 +1,13 @@
-import java.util.*;
-
 class Solution {
-    public int minimumOperations(int[] n) {
-        if (isd(n)) return 0;
-        int c = 0;
-        while (!isd(n)) {
-            n = rm(n);
-            c++;
-        }
-        return c;
-    }
 
-    public boolean isd(int[] n) {
-        Set<Integer> set = new HashSet<>();
-        for (int val : n) {
-            if (!set.add(val)) return false;
+    public int minimumOperations(int[] nums) {
+        boolean[] seen = new boolean[128];
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (seen[nums[i]]) {
+                return i / 3 + 1;
+            }
+            seen[nums[i]] = true;
         }
-        return true;
-    }
-
-    public int[] rm(int[] n) {
-        if (n.length <= 3) return new int[]{};
-        int[] p = new int[n.length - 3];
-        for (int i = 3; i < n.length; i++) {
-            p[i - 3] = n[i];
-        }
-        return p;
+        return 0;
     }
 }
